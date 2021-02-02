f37d24016966ac9ff32bab336517af99632bf388
Miguel <gannasong@gmail.com>
2021-02-02 16:46:26 +0800

    finish basic layout and stacks

--- DesignCode/ContentView.swift
+++ DesignCode/ContentView.swift
@@ -8,14 +8,46 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
+  var body: some View {
+    ZStack {
+      VStack {
+        Spacer()
+      }.frame(width: 300, height: 220)
+      .background(Color.blue)
+      .cornerRadius(20)
+      .shadow(radius: 20)
+      .offset(x: 0, y: -20)
+
+      VStack {
+        HStack {
+          VStack(alignment: .leading) {
+            Text("UI Design")
+              .font(.title)
+              .fontWeight(.semibold)
+              .foregroundColor(.white)
+            Text("Certificates")
+              .foregroundColor(Color("accent"))
+          }
+          Spacer()
+          Image("Logo1")
+        }
+        .padding()
+        Spacer()
+        Image("Card1")
+          .resizable()
+          .aspectRatio(contentMode: .fill)
+          .frame(width: 300, height: 110, alignment: .top)
+      }
+      .frame(width: 340.0, height: 220.0)
+      .background(Color.black)
+      .cornerRadius(20)
+      .shadow(radius: 20)
     }
+  }
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+  static var previews: some View {
+    ContentView()
+  }
 }

7bfbf72a87357c377de5bfe30f9a0065f5fad972
Miguel <gannasong@gmail.com>
2021-02-02 23:37:01 +0800

    建立 BottomCardView

--- DesignCode/ContentView.swift
+++ DesignCode/ContentView.swift
@@ -11,6 +11,7 @@ struct ContentView: View {
   var body: some View {
     ZStack {
       TitleView()
+        .blur(radius: 20)
 
       BackCardView()
         .background(Color("card4"))
@@ -34,6 +35,9 @@ struct ContentView: View {
 
       CardView()
         .blendMode(.hardLight)
+
+      BottomCardView()
+        .blur(radius: 20)
     }
   }
 }
@@ -94,5 +98,28 @@ struct TitleView: View {
       Image("Background1")
       Spacer()
     }
+  }
+}
+
+struct BottomCardView: View {
+  var body: some View {
+    VStack(spacing: 20) {
+      Rectangle()
+        .frame(width: 40, height: 5)
+        .cornerRadius(3)
+        .opacity(0.1)
+      Text("This certificate is proof that Meng To has achieved the UI Design course with approval from a Design+Code instructor.")
+        .multilineTextAlignment(.center)
+        .font(.subheadline)
+        .lineSpacing(4)
+      Spacer()
+    }
+    .padding(.top, 8)
+    .padding(.horizontal, 20)
+    .frame(maxWidth: .infinity)
+    .background(Color.white)
+    .cornerRadius(30)
+    .shadow(radius: 20)
+    .offset(x: 0, y: 600)
   }
 }

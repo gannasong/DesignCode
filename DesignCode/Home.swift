1b92c5971c03da1b7880b3e52a901eeebdbed0b1
Miguel <gannasong@gmail.com>
2021-02-04 16:56:15 +0800

    create Home view

---
+++ DesignCode/Home.swift
@@ -0,0 +1,20 @@
+//
+//  Home.swift
+//  DesignCode
+//
+//  Created by SUNG HAO LIN on 2021/2/4.
+//
+
+import SwiftUI
+
+struct Home: View {
+  var body: some View {
+    Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+  }
+}
+
+struct Home_Previews: PreviewProvider {
+  static var previews: some View {
+    Home()
+  }
+}

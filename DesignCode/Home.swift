54f6ca29b63a2d5c140b74f6ce16b6305dd8c427
Miguel <gannasong@gmail.com>
2021-02-04 22:30:23 +0800

    add drag progress

--- DesignCode/Home.swift
+++ DesignCode/Home.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct Home: View {
   @State var showProfile = false
+  @State var viewState: CGSize = .zero
 
   var body: some View {
     ZStack {
@@ -41,14 +42,30 @@ struct Home: View {
       .clipShape(RoundedRectangle(cornerRadius: 30, style: .continuous))
       .shadow(color: .black.opacity(0.2), radius: 20, x: 0, y: 20) // 左下，隱影記得要加在切圓角之後
       .offset(y: showProfile ? -450 : 0)
-      .rotation3DEffect(.degrees(showProfile ? -10 : 0), axis: (x: 10, y: 0, z: 0)) // 3D 效果
+      .rotation3DEffect(.degrees(showProfile ? Double(viewState.height / 10) - 10 : 0), axis: (x: 10, y: 0, z: 0)) // 3D 效果
       .scaleEffect(showProfile ? 0.9 : 1) // 縮放大小
       .animation(.spring(response: 0.5, dampingFraction: 0.6, blendDuration: 0)) // 添加動畫
       .edgesIgnoringSafeArea(.all)
 
       MenuView()
-        .offset(y: showProfile ? 0 : 600)
+        .background(Color.black.opacity(0.001)) //＊＊＊技巧： 設為 0.1 會喪失互性，但 0.001 會保持互動性 ＊＊＊
+        .offset(y: showProfile ? 0 : 1000) // 1000 是往下推
+        .offset(y: viewState.height)
         .animation(.spring(response: 0.5, dampingFraction: 0.6, blendDuration: 0))
+        .onTapGesture {
+          showProfile.toggle()
+        }
+        .gesture(
+          DragGesture().onChanged { value in
+            viewState = value.translation
+          }
+          .onEnded{ value in
+            if viewState.height > 50 { // 往下拉的時候自動關閉
+              showProfile = false
+            }
+            viewState = .zero
+          }
+        )
     }
   }
 }

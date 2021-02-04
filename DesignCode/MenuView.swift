0da296428dbfba18dcdef2a54d1e51c0003632dc
Miguel <gannasong@gmail.com>
2021-02-04 15:06:03 +0800

    extract MenuRow subview

--- DesignCode/MenuView.swift
+++ DesignCode/MenuView.swift
@@ -9,12 +9,27 @@ import SwiftUI
 
 struct MenuView: View {
   var body: some View {
-    Text("Account")
+    VStack {
+      MenuRow()
+    }
   }
 }
 
 struct MenuView_Previews: PreviewProvider {
   static var previews: some View {
     MenuView()
+  }
+}
+
+struct MenuRow: View {
+  var body: some View {
+    HStack(spacing: 16) { // 設定 spacing 讓 Image 跟 Text 間距加大
+      Image(systemName: "gear")
+        .font(.system(size: 20, weight: .light))
+        .imageScale(.large) // 圖案變大
+
+      Text("Account")
+        .font(.system(size: 20, weight: .bold, design: .default)) // design 就是下載的字體
+    }
   }
 }

c18db7c87111a90db40a478ce1316e7f7453e8a1
Miguel <gannasong@gmail.com>
2021-02-05 00:12:45 +0800

    add scrollView

--- DesignCode/HomeView.swift
+++ DesignCode/HomeView.swift
@@ -21,7 +21,18 @@ struct HomeView: View {
         AvatarView(showProfile: $showProfile) // 傳入的 State 必須要加上 $ 符號
       }
       .padding(.horizontal)
+      .padding(.leading, 14)
       .padding(.top, 30)
+
+      ScrollView(.horizontal, showsIndicators: false) {
+        HStack(spacing: 30) { // item 之間間隔
+          ForEach(0 ..< 5) { item in
+            SectionView()
+          }
+        }
+        .padding(30)
+        .padding(.bottom, 30)
+      }
 
       Spacer()
     }
@@ -31,5 +42,34 @@ struct HomeView: View {
 struct HomeView_Previews: PreviewProvider {
   static var previews: some View {
     HomeView(showProfile: .constant(false)) // 如果有 di 的參數，不需給假資料，prview 可給預設參數
+  }
+}
+
+struct SectionView: View {
+  var body: some View {
+    VStack {
+      HStack(alignment: .top) {
+        Text("Prototype designs in SwiftUI")
+          .font(.system(size: 24, weight: .bold))
+          .frame(width: 160, alignment: .leading)
+          .foregroundColor(.white)
+        Spacer()
+        Image("Logo1")
+      }
+
+      Text("18 Sections".uppercased()) // 把字變大寫
+        .frame(maxWidth: .infinity, alignment: .leading)
+
+      Image("Card1")
+        .resizable()
+        .aspectRatio(contentMode: .fit)
+        .frame(width: 210)
+    }
+    .padding(.top, 20)
+    .padding(.horizontal, 20)
+    .frame(width: 275, height: 275)
+    .background(Color("card1"))
+    .cornerRadius(30)
+    .shadow(color: Color("card1").opacity(0.3), radius: 20, x: 0, y: 20)
   }
 }

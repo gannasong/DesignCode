5bc8a531fd9dd0a99f2fc54c3e1bbbf38b15598d
Miguel <gannasong@gmail.com>
2021-02-04 14:28:55 +0800

    create MenuView

---
+++ DesignCode/MenuView.swift
@@ -0,0 +1,20 @@
+//
+//  MenuView.swift
+//  DesignCode
+//
+//  Created by SUNG HAO LIN on 2021/2/4.
+//
+
+import SwiftUI
+
+struct MenuView: View {
+  var body: some View {
+    Text("Account")
+  }
+}
+
+struct MenuView_Previews: PreviewProvider {
+  static var previews: some View {
+    MenuView()
+  }
+}

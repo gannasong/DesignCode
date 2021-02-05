205c3f9a293139e9088ac56e02f3cd4b47fa50f9
Miguel <gannasong@gmail.com>
2021-02-05 23:48:07 +0800

    create UpdateDetail view

---
+++ DesignCode/UpdateDetail.swift
@@ -0,0 +1,20 @@
+//
+//  UpdateDetail.swift
+//  DesignCode
+//
+//  Created by SUNG HAO LIN on 2021/2/5.
+//
+
+import SwiftUI
+
+struct UpdateDetail: View {
+  var body: some View {
+    Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+  }
+}
+
+struct UpdateDetail_Previews: PreviewProvider {
+  static var previews: some View {
+    UpdateDetail()
+  }
+}

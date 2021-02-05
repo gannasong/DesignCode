e373e66446e733d55468114364d2cdfbffae05cc
Miguel <gannasong@gmail.com>
2021-02-05 14:53:21 +0800

    create updatelist view

---
+++ DesignCode/UpdateList.swift
@@ -0,0 +1,20 @@
+//
+//  UpdateList.swift
+//  DesignCode
+//
+//  Created by SUNG HAO LIN on 2021/2/5.
+//
+
+import SwiftUI
+
+struct UpdateList: View {
+  var body: some View {
+    Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+  }
+}
+
+struct UpdateList_Previews: PreviewProvider {
+  static var previews: some View {
+    UpdateList()
+  }
+}

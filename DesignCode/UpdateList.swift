671ed059e24feb88d5f9c15c4490549e48303197
Miguel <gannasong@gmail.com>
2021-02-05 17:56:16 +0800

    create navigationView

--- DesignCode/UpdateList.swift
+++ DesignCode/UpdateList.swift
@@ -12,7 +12,31 @@ struct UpdateList: View {
     NavigationView {
       List(updateData) { update in
         NavigationLink(destination: Text(update.text)) {
-          Text(update.title)
+          HStack {
+            Image(update.image)
+              .resizable()
+              .aspectRatio(contentMode: .fit)
+              .frame(width: 80, height: 80)
+              .background(Color.black)
+              .cornerRadius(20)
+              .padding(.trailing, 4)
+
+            VStack(alignment: .leading, spacing: 8.0) {
+              Text(update.title)
+                .font(.system(size: 20, weight: .bold))
+
+              Text(update.text)
+                .lineLimit(2)
+                .font(.subheadline)
+                .foregroundColor(Color(#colorLiteral(red: 0.501960814, green: 0.501960814, blue: 0.501960814, alpha: 1)))
+
+              Text(update.date)
+                .font(.caption)
+                .fontWeight(.bold)
+                .foregroundColor(.secondary)
+            }
+          }
+          .padding(.vertical, 8)
         }
       }
       .navigationTitle(Text("Updates"))

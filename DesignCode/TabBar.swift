afc86215afb0c79ab0a7439e58a7a28a4f995c18
Miguel <gannasong@gmail.com>
2021-02-06 19:44:29 +0800

    make TabBar

--- DesignCode/TabBar.swift
+++ DesignCode/TabBar.swift
@@ -9,12 +9,24 @@ import SwiftUI
 
 struct TabBar: View {
   var body: some View {
-    Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    TabView {
+      Home().tabItem {
+        Image(systemName: "play.circle.fill")
+        Text("Home")
+      }
+      ContentView().tabItem {
+        Image(systemName: "rectangle.stack.fill")
+        Text("Certificates")
+      }
+    }
+    .edgesIgnoringSafeArea(.top)
   }
 }
 
 struct TabBar_Previews: PreviewProvider {
   static var previews: some View {
     TabBar()
+//    TabBar().previewDevice("iPhone 8") // 使用 previewDevice 可以看指定的尺寸
+    // 使用 Group 可以同時 preview 多裝置
   }
 }

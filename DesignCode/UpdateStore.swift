5c4de4b4adb530071584024c833d7c41e79399bc
Miguel <gannasong@gmail.com>
2021-02-06 09:53:33 +0800

    create UpdateStore file

---
+++ DesignCode/UpdateStore.swift
@@ -0,0 +1,20 @@
+//
+//  UpdateStore.swift
+//  DesignCode
+//
+//  Created by SUNG HAO LIN on 2021/2/6.
+//
+
+import SwiftUI
+
+struct UpdateStore: View {
+  var body: some View {
+    Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+  }
+}
+
+struct UpdateStore_Previews: PreviewProvider {
+  static var previews: some View {
+    UpdateStore()
+  }
+}

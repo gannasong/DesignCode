65ba9da7e3686c3e6cc5917cfc3fdf92da8c1eb6
Miguel <gannasong@gmail.com>
2021-02-06 10:44:03 +0800

    create TabBar view

---
+++ DesignCode/TabBar.swift
@@ -0,0 +1,20 @@
+//
+//  TabBar.swift
+//  DesignCode
+//
+//  Created by SUNG HAO LIN on 2021/2/6.
+//
+
+import SwiftUI
+
+struct TabBar: View {
+  var body: some View {
+    Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+  }
+}
+
+struct TabBar_Previews: PreviewProvider {
+  static var previews: some View {
+    TabBar()
+  }
+}

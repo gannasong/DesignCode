0f0b0c66d9d6ea7c461b112146081d9268507140
Miguel <gannasong@gmail.com>
2021-02-07 16:28:01 +0800

    add chapter 22.md

--- DesignCode/RingView.swift
+++ DesignCode/RingView.swift
@@ -8,25 +8,34 @@
 import SwiftUI
 
 struct RingView: View {
+  var color1 = #colorLiteral(red: 0.8078431487, green: 0.02745098062, blue: 0.3333333433, alpha: 1)
+  var color2 = #colorLiteral(red: 0.3647058904, green: 0.06666667014, blue: 0.9686274529, alpha: 1)
+  var width: CGFloat = 44
+  var height: CGFloat = 44
+  var percent: CGFloat = 88
+
   var body: some View {
-    ZStack {
+    let multiplier: CGFloat = width / 44
+    let progress = 1 - (percent / 100)
+
+    return ZStack {
       Circle() // 底部的循環背景
-        .stroke(Color.black.opacity(0.1), style: StrokeStyle(lineWidth: 5))
-        .frame(width: 44, height: 44)
+        .stroke(Color.black.opacity(0.1), style: StrokeStyle(lineWidth: 5 * multiplier))
+        .frame(width: width, height: height)
 
       Circle()
-        .trim(from: 0.2, to: 1)
+        .trim(from: progress, to: 1)
         .stroke(
-          LinearGradient(gradient: Gradient(colors: [Color(#colorLiteral(red: 0.8078431487, green: 0.02745098062, blue: 0.3333333433, alpha: 1)), Color(#colorLiteral(red: 0.3647058904, green: 0.06666667014, blue: 0.9686274529, alpha: 1))]), startPoint: .topTrailing, endPoint: .bottomLeading),
-          style: StrokeStyle(lineWidth: 5, lineCap: .round, lineJoin: .round, miterLimit: .infinity, dash: [20, 0], dashPhase: 0)
+          LinearGradient(gradient: Gradient(colors: [Color(color1), Color(color2)]), startPoint: .topTrailing, endPoint: .bottomLeading),
+          style: StrokeStyle(lineWidth: 5 * multiplier, lineCap: .round, lineJoin: .round, miterLimit: .infinity, dash: [20, 0], dashPhase: 0)
         )
         .rotationEffect(.degrees(90)) // 左右旋轉
         .rotation3DEffect(.degrees(180), axis: (x: 1, y: 0, z: 0))
-        .frame(width: 44, height: 44)
-        .shadow(color: Color(#colorLiteral(red: 0.3647058904, green: 0.06666667014, blue: 0.9686274529, alpha: 1)).opacity(0.1), radius: 3, x: 0, y: 3)
+        .frame(width: width, height: height)
+        .shadow(color: Color(color2).opacity(0.1), radius: 3 * multiplier, x: 0, y: 3 * multiplier)
 
-      Text("82%")
-        .font(.subheadline)
+      Text("\(Int(percent))%")
+        .font(.system(size: 14 * multiplier))
         .fontWeight(.bold)
     }
   }
